c34c46e23a29164589f92a9f8e6782dde223e383
vivian <vivian.govender@live.com>
2023-06-06 08:58:02 -0700

    t03

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for leters in range(ord('a'), ord('z') + 1):
     if chr(leters) != 'q' and chr(leters) != 'e':
-        print("{:c}".format(a), end='')
+        print("{:c}".format(leters), end='')
