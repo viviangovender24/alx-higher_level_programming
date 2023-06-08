eca99f7aa5c3108378954a1a52a1ef95b999812f
vivian <vivian.govender@live.com>
2023-06-08 09:09:14 -0700

    t02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    # number and list
+    import sys
+    c = len(sys.argv) - 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+
+    for i in range(c):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
