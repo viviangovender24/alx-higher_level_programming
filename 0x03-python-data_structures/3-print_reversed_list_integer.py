9e98f13b89e55cb47306d8b13c99d0859c3f6f19
vivian <vivian.govender@live.com>
2023-06-12 02:31:59 -0700

    t3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    # Print integers of a list in revers
+    if isinstance(my_list, list):
+        my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
