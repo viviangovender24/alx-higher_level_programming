f1622d7da2ce2374ba3f413e2ebfa05c090d9aaf
vivian <vivian.govender@live.com>
2023-06-12 00:11:23 -0700

    t1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    # Retrive an element
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-        return (my_list[idx])
+    le = len(my_list)
+    return (my_list[idx] if 0 <= idx < le else None)
