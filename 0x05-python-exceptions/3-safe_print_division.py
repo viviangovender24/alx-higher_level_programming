fa51dcdba2df7c6909544a1321190b641c471a2b
vivian <vivian.govender@live.com>
2023-06-25 22:13:17 -0700

    t0_t07

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
+def safe_print_division(a, b):  # divide
     try:
         res = a / b
     except ZeroDivisionError:
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):  # divide element
     new_l = []
     for i in range(list_length):
         try:
